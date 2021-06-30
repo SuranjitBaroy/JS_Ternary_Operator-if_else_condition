f07664e29526391e98e1fcd096b4d5560700ce6a
Suranjit Baroy <62200368+SuranjitBaroy@users.noreply.github.com>
2021-06-30 08:27:51 +0600

    Add files via upload

---
+++ learn.js
@@ -0,0 +1,57 @@
+
+//Comparison Operators
+
+var one = 20;
+var two = 20;
+
+console.log(one == two);
+
+//===
+
+var one = 20;
+var two = "20";
+
+console.log(one === two);
+
+
+//!=
+
+var one = 20;
+var two = 13;
+
+console.log(one != two);
+
+//!==
+
+var one = 20;
+var two = 13;
+
+console.log(one != two);
+
+//>,<,>=,<=
+
+//Logical Operator &&,||
+
+console.log(true && false);
+console.log(true || true);
+console.log(!false);
+
+//Ternary Operator
+
+var ONE = 10;
+var TWO = 20;
+
+var Three = ONE>TWO ? 100:200;
+
+console.log(Three);
+
+//Please Visit this site JS Operator Presedence
+
+//Finally JS Condition if, else , elseif
+
+
+
+
+
+
+
